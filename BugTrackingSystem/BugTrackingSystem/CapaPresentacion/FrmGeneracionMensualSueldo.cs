b3507c2c7317eb1805e57fe37af14e71e7796a77
Fran Merlini <merlinifrancisco@gmail.com>
2021-10-09 23:25:06 -0300

    Frm transaccion sueldo -> btn agregar listo

--- BugTrackingSystem/BugTrackingSystem/CapaPresentacion/FrmGeneracionMensualSueldo.cs
+++ BugTrackingSystem/BugTrackingSystem/CapaPresentacion/FrmGeneracionMensualSueldo.cs
@@ -24,8 +24,7 @@ namespace BugTrackingSystem.CapaPresentacion
         public FrmGeneracionMensualSueldo()
         {
             InitializeComponent();
-            dgvAsignaciones.AutoGenerateColumns = false;
-            dgvDescuentos.AutoGenerateColumns = false;
+            InitializeDataGridView();
 
             usuarioService = new UsuarioService();
             sueldoService = new SueldoService();
@@ -40,9 +39,47 @@ namespace BugTrackingSystem.CapaPresentacion
         {
             LlenarCombo(cboUsuario, usuarioService.ObtenerUsuarios(), "Nombre");
 
-            //rbAsignacion.Checked = true;
-
+            dgvAsignaciones.DataSource = listaSueldoAsignacion;
+            dgvDescuentos.DataSource = listaSueldoDescuento;
         }
+
+        private void InitializeDataGridView()
+        {
+            // Cree un DataGridView no vinculado declarando un recuento de columnas.
+            dgvAsignaciones.ColumnCount = 2;
+            dgvAsignaciones.ColumnHeadersVisible = true;
+            dgvDescuentos.ColumnCount = 2;
+            dgvDescuentos.ColumnHeadersVisible = true;
+
+            // Configuramos la AutoGenerateColumns en false para que no se autogeneren las columnas
+            dgvAsignaciones.AutoGenerateColumns = false;
+            dgvDescuentos.AutoGenerateColumns = false;
+
+            // Cambia el estilo de la cabecera de la grilla.
+            DataGridViewCellStyle columnHeaderStyle = new DataGridViewCellStyle
+            {
+                BackColor = Color.Beige,
+                Font = new Font("Verdana", 8, FontStyle.Bold)
+            };
+            dgvAsignaciones.ColumnHeadersDefaultCellStyle = columnHeaderStyle;
+            dgvDescuentos.ColumnHeadersDefaultCellStyle = columnHeaderStyle;
+
+            // Definimos el nombre de la columnas y el DataPropertyName que se asocia a DataSource
+
+            CrearColumnas(dgvAsignaciones, 0, "Observación", "Asignacion", 210);
+            CrearColumnas(dgvAsignaciones, 1, "Monto", "Monto", 120);
+            CrearColumnas(dgvDescuentos, 0, "Observación", "Descuento", 210);
+            CrearColumnas(dgvDescuentos, 1, "Monto", "Monto", 120);
+        }
+
+        private void CrearColumnas(DataGridView tabla, int columna, string nombre, string propiedad, int tamaño)
+        {
+            tabla.Columns[columna].Name = nombre;
+            tabla.Columns[columna].DataPropertyName = propiedad;
+            tabla.Columns[columna].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            tabla.Columns[columna].Width = tamaño;
+        }
+
         private void LlenarCombo(ComboBox cbx, Object source, string display)
         {
             // Datasource: establece el origen de datos de este objeto.
@@ -55,22 +92,60 @@ namespace BugTrackingSystem.CapaPresentacion
 
         private void btnAgregar_Click(object sender, EventArgs e)
         {
+
+            if (cboDtoAsig.SelectedItem == null)
+            {
+                MessageBox.Show("Debe elegir una asignación/descuento", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
             if (rbAsignacion.Checked)
             {
+                listaSueldoAsignacion.Add(new SueldoAsignacion()
+                {
+                    Usuario = (Usuario)cboUsuario.SelectedItem,
+                    Fecha = (DateTime)dtpFecha.Value,
+                    Asignacion = (Asignacion)cboDtoAsig.SelectedItem,
+                    Monto = Convert.ToDecimal(txtImporte.Text),
+                    Cantidad = Convert.ToInt32(nudCantidad.Value)
+                });
             }
             else if (rbDescuento.Checked)
             {
-
+                listaSueldoDescuento.Add(new SueldoDescuento()
+                {
+                    Usuario = (Usuario)cboUsuario.SelectedItem,
+                    Fecha = (DateTime)dtpFecha.Value,
+                    Descuento = (Descuento)cboDtoAsig.SelectedItem,
+                    Monto = Convert.ToDecimal(txtImporte.Text),
+                    Cantidad = Convert.ToInt32(nudCantidad.Value)
+                });
             }
 
+            CalcularTotales();
+            cboDtoAsig.SelectedIndex = -1;
+        }
+
+        private void CalcularTotales()
+        {
+            decimal totalAsignaciones = listaSueldoAsignacion.Sum(a => a.Monto);
+            txtAsigSueldo.Text = totalAsignaciones.ToString("C");
+
+            decimal totalDescuentos = listaSueldoDescuento.Sum(d => d.Monto);
+            txtDtoSueldo.Text = totalDescuentos.ToString("C");
+
+            var sueldoBruto = nudSueldoBruto.Value;
+
+            var importeTotal = sueldoBruto + totalAsignaciones - totalDescuentos;
+            txtSueldoTotal.Text = importeTotal.ToString("C");
         }
 
         private void cboDtoAsig_SelectedIndexChanged(object sender, EventArgs e)
         {
-            CargarMontoEImporte();
+            ActualizarMontoEImporte();
         }
 
-        private void CargarMontoEImporte()
+        private void ActualizarMontoEImporte()
         {
             if (cboDtoAsig.SelectedIndex == -1)
             {
@@ -95,19 +170,24 @@ namespace BugTrackingSystem.CapaPresentacion
 
         private void nudCantidad_ValueChanged(object sender, EventArgs e)
         {
-            CargarMontoEImporte();
+            ActualizarMontoEImporte();
         }
 
         private void rbDescuento_Click(object sender, EventArgs e)
         {
             LlenarCombo(cboDtoAsig, descuentoService.ObtenerDescuentos(), "Nombre");
-            CargarMontoEImporte();
+            ActualizarMontoEImporte();
         }
 
         private void rbAsignacion_Click(object sender, EventArgs e)
         {
             LlenarCombo(cboDtoAsig, asignacionService.ObtenerAsignaciones(), "Nombre");
-            CargarMontoEImporte();
+            ActualizarMontoEImporte();
+        }
+
+        private void nudSueldoBruto_ValueChanged(object sender, EventArgs e)
+        {
+            CalcularTotales();
         }
     }
 }
